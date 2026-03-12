b46f1ba30e7dded2f228666ea292ec14b423a6e2
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2026-03-12 00:03:12 -0300

    ADD: Star movemente
    Added star fild movement. TODO: Make speed relative to the Ship speed (speed increses with time)

--- Fintech_Invaders/Fintech_Invaders.ino
+++ Fintech_Invaders/Fintech_Invaders.ino
@@ -4,6 +4,11 @@
 #define PLAY_AREA_LEFT   59
 #define PLAY_AREA_RIGHT  259
 
+#define STAR0_COUNT 10
+#define STAR1_COUNT 7
+#define STAR2_COUNT 3
+#define STAR_SPEED 8  // at the future, must be relative to spaceship speed.
+
 using fabgl::iclamp;
 
 fabgl::VGAController DisplayController;
@@ -13,13 +18,11 @@ fabgl::Canvas canvas(&DisplayController);
 
 struct GameScene : public Scene {
 
-  static const int SPRITESCOUNT = 12;
+  static const int SPRITESCOUNT = 22;
   fabgl::Sprite sprites[SPRITESCOUNT];
-  fabgl::Sprite* player = &sprites[0];       //player sprite group
-  fabgl::Sprite* afterburner = &sprites[5];  //afterburner sprites
-  fabgl::Sprite* star0 = &sprites[9];        //star1 sprites
-  fabgl::Sprite* star1 = &sprites[10];        //star2 sprites
-  fabgl::Sprite* star2 = &sprites[11];        //star3 sprites
+
+  fabgl::Sprite* player = &sprites[20];
+  fabgl::Sprite* afterburner = &sprites[21];
 
   int playerVelX = 0;
   int lastAnimUpdate = 0;
@@ -29,55 +32,134 @@ struct GameScene : public Scene {
   GameScene()
     : Scene(SPRITESCOUNT, 20, DisplayController.getViewPortWidth(), DisplayController.getViewPortHeight()) {}
 
+
+
+  // ----------- STAR INITIALIZATION -----------
+  void initStars() {
+
+    int index = 0;
+
+    // small stars
+    for (int i = 0; i < STAR0_COUNT; i++) {
+      sprites[index].addBitmap(&starBMP0);
+      sprites[index].moveTo(
+        random(PLAY_AREA_LEFT + 1, PLAY_AREA_RIGHT - 3),
+        random(0, DisplayController.getViewPortHeight())
+      );
+      addSprite(&sprites[index]);
+      index++;
+    }
+
+    // medium stars
+    for (int i = 0; i < STAR1_COUNT; i++) {
+      sprites[index].addBitmap(&starBMP1);
+      sprites[index].moveTo(
+        random(PLAY_AREA_LEFT + 1, PLAY_AREA_RIGHT - 5),
+        random(0, DisplayController.getViewPortHeight())
+      );
+      addSprite(&sprites[index]);
+      index++;
+    }
+
+    // large stars
+    for (int i = 0; i < STAR2_COUNT; i++) {
+      sprites[index].addBitmap(&starBMP2);
+      sprites[index].moveTo(
+        random(PLAY_AREA_LEFT + 1, PLAY_AREA_RIGHT - 7),
+        random(0, DisplayController.getViewPortHeight())
+      );
+      addSprite(&sprites[index]);
+      index++;
+    }
+  }
+
+  // ----------- STAR MOVEMENT -----------
+  void updateStars() {
+
+    int start = 0;
+    int end = STAR0_COUNT + STAR1_COUNT + STAR2_COUNT;
+
+    for (int i = start; i < end; i++) {
+
+      sprites[i].y += STAR_SPEED;
+
+      if (sprites[i].y >= DisplayController.getViewPortHeight()) {
+
+        sprites[i].y = 0;
+
+        sprites[i].x = random(
+          PLAY_AREA_LEFT + 1,
+          PLAY_AREA_RIGHT - sprites[i].getWidth()
+        );
+      }
+
+      updateSprite(&sprites[i]);
+    }
+  }
+
+
+
   void init() {
 
     canvas.clear();
 
+    // PLAYER BITMAPS
     player->addBitmap(&bitmap1);
     player->addBitmap(&bitmap2);
-    player->addBitmap(&bitmap3);  //Main sprite for player
+    player->addBitmap(&bitmap3);
     player->addBitmap(&bitmap4);
     player->addBitmap(&bitmap5);
 
-    //sprites for afterburner
+    // AFTERBURNER
     afterburner->addBitmap(&afterburner_0);
     afterburner->addBitmap(&afterburner_1);
     afterburner->addBitmap(&afterburner_2);
     afterburner->addBitmap(&afterburner_3);
 
-    //sprites for Starts
+    player->moveTo(152,170);
+    afterburner->moveTo(156,173);
 
-    star0->addBitmap(&starBMP0);
-    star1->addBitmap(&starBMP1);
-    star2->addBitmap(&starBMP2);
+    // STARFIELD
+    initStars();
 
-    star0->moveTo((320-3)/2,100);
-    star1->moveTo((320-5)/2,110);
-    star2->moveTo((320-7)/2,120);
-
-    afterburner->moveTo(152 + 4, 170 + 4 - 1);  //test
-
-    player->moveTo(152, 170);  //x,y
-
-
+    // PLAYER + ENGINE
     addSprite(player);
     addSprite(afterburner);
-    addSprite(star0);
-    addSprite(star1);
-    addSprite(star2);
+
     DisplayController.setSprites(sprites, SPRITESCOUNT);
+
     player->setFrame(2);
     afterburner->setFrame(0);
 
-  //DRAW THE RIGHT AND LEFT MENUS
-  canvas.setBrushColor(Color::White);
-  //canvas.setBrushColor(3, 2, 0);
-  canvas.fillRectangle(0,0, PLAY_AREA_LEFT,DisplayController.getViewPortHeight() - 1 );
-  canvas.fillRectangle( PLAY_AREA_RIGHT, 0, DisplayController.getViewPortWidth() - 1, DisplayController.getViewPortHeight() - 1); 
-  //DRAW THE STAR FIELD
-  canvas.setBrushColor(Color::Black);
-  canvas.fillRectangle(PLAY_AREA_LEFT + 1, 0, PLAY_AREA_RIGHT -1 , DisplayController.getViewPortHeight() - 1);
+
+    // SIDE PANELS
+    canvas.setBrushColor(Color::White);
+
+    canvas.fillRectangle(
+      0,0,
+      PLAY_AREA_LEFT,
+      DisplayController.getViewPortHeight() - 1
+    );
+
+    canvas.fillRectangle(
+      PLAY_AREA_RIGHT,
+      0,
+      DisplayController.getViewPortWidth() - 1,
+      DisplayController.getViewPortHeight() - 1
+    );
+
+    // STARFIELD BACKGROUND
+    canvas.setBrushColor(Color::Black);
+
+    canvas.fillRectangle(
+      PLAY_AREA_LEFT + 1,
+      0,
+      PLAY_AREA_RIGHT - 1,
+      DisplayController.getViewPortHeight() - 1
+    );
   }
+
+
 
   void update(int updateCount) override {
 
@@ -88,107 +170,145 @@ struct GameScene : public Scene {
     bool rightPressed = false;
 
     if (keyboard && keyboard->isKeyboardAvailable()) {
+
       if (keyboard->isVKDown(fabgl::VK_LEFT)) {
         playerVelX = -2;
         leftPressed = true;
-      } else if (keyboard->isVKDown(fabgl::VK_RIGHT)) {
+      }
+
+      else if (keyboard->isVKDown(fabgl::VK_RIGHT)) {
         playerVelX = 2;
         rightPressed = true;
       }
     }
 
-    // move player
-    player->x += playerVelX;
-    player->x = iclamp(player->x,PLAY_AREA_LEFT + 1, PLAY_AREA_RIGHT - player->getWidth());
 
-    // animation
+    // PLAYER MOVEMENT
+    player->x += playerVelX;
+
+    player->x = iclamp(
+      player->x,
+      PLAY_AREA_LEFT + 1,
+      PLAY_AREA_RIGHT - player->getWidth()
+    );
+
+
+    // PLAYER ANIMATION
     if (rightPressed) {
+
       if (!rightHeld) {
         rightHeld = true;
         lastAnimUpdate = updateCount;
       }
+
       if (updateCount - lastAnimUpdate >= 4) {
+
         lastAnimUpdate = updateCount;
+
         int frame = player->getFrameIndex();
-        if (frame < 4) player->setFrame(frame + 1);  // advance to left tilt
+
+        if (frame < 4)
+          player->setFrame(frame + 1);
       }
-    } else {
-      rightHeld = false;
-    }
+
+    } else rightHeld = false;
+
+
 
     if (leftPressed) {
+
       if (!leftHeld) {
         leftHeld = true;
         lastAnimUpdate = updateCount;
       }
-      if (updateCount - lastAnimUpdate >= 4) {
-        lastAnimUpdate = updateCount;
-        int frame = player->getFrameIndex();
-        if (frame > 0) player->setFrame(frame - 1);  // advance to right tilt
-      }
-    } else {
-      leftHeld = false;
-    }
 
-    // if no keys, return to neutral
+      if (updateCount - lastAnimUpdate >= 4) {
+
+        lastAnimUpdate = updateCount;
+
+        int frame = player->getFrameIndex();
+
+        if (frame > 0)
+          player->setFrame(frame - 1);
+      }
+
+    } else leftHeld = false;
+
+
+
+    // RETURN TO CENTER
     if (!leftPressed && !rightPressed) {
 
       if (updateCount - lastAnimUpdate >= 4) {
+
         lastAnimUpdate = updateCount;
+
         int frame = player->getFrameIndex();
-        if (frame < 2) player->setFrame(frame + 1);  // advance to right tilt
-        if (frame > 2) player->setFrame(frame - 1);  // advance to right tilt
+
+        if (frame < 2) player->setFrame(frame + 1);
+        if (frame > 2) player->setFrame(frame - 1);
       }
-      //player->setFrame(2);
     }
 
-    // animate afterburner
-if (updateCount % 4 == 0) {
 
-  static int lastFrame = 0;
 
-  int newFrame = random(0, 4);
+    // AFTERBURNER ANIMATION
+    if (updateCount % 4 == 0) {
 
-  lastFrame = (newFrame == lastFrame) ? random(0, 4) : newFrame;
+      static int lastFrame = 0;
 
-  afterburner->setFrame(lastFrame);
-}
-    // keep it attached to player
+      int newFrame = random(0,4);
+
+      lastFrame = (newFrame == lastFrame) ? random(0,4) : newFrame;
+
+      afterburner->setFrame(lastFrame);
+    }
+
+
+    // ATTACH AFTERBURNER
     afterburner->x = player->x + 6;
     afterburner->y = player->y + (16 - 3);
-    // update sprites
-    updateSprite(star0);
-    updateSprite(star1);
-    updateSprite(star2);
+
+
+    // UPDATE STARFIELD
+    updateStars();
+
+
+    // UPDATE PLAYER
     updateSprite(player);
     updateSprite(afterburner);
+
     DisplayController.refreshSprites();
   }
 
+
+
   void collisionDetected(Sprite* spriteA, Sprite* spriteB, Point collisionPoint) override {
-    // handle collisions here
   }
 };
-
-
-
 
 
 
 GameScene* gameScene;
 
 
+
 void setup() {
 
-  PS2Controller.begin(PS2Preset::KeyboardPort0, KbdMode::GenerateVirtualKeys);
+  PS2Controller.begin(
+    PS2Preset::KeyboardPort0,
+    KbdMode::GenerateVirtualKeys
+  );
 
   DisplayController.begin();
   DisplayController.setResolution(VGA_320x200_75Hz);
-  DisplayController.moveScreen(21, -1);
+
+  DisplayController.moveScreen(21,-1);
 
   gameScene = new GameScene();
   gameScene->start();
 }
+
 
 
 void loop() {
