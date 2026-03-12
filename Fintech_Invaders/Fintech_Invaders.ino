9fce0732181e5ec85bb3c21db459130d9ef2ff70
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2026-03-11 21:36:03 -0300

    ADD: afterburner sprite + animation
    added a afterburner to the ship, and its animated

--- Fintech_Invaders/Fintech_Invaders.ino
+++ Fintech_Invaders/Fintech_Invaders.ino
@@ -10,9 +10,10 @@ fabgl::Canvas canvas(&DisplayController);
 
 struct GameScene : public Scene {
 
-  static const int SPRITESCOUNT = 1;
+  static const int SPRITESCOUNT = 9;
   fabgl::Sprite sprites[SPRITESCOUNT];
-  fabgl::Sprite* player = &sprites[0];
+  fabgl::Sprite* player = &sprites[0];       //player sprite group
+  fabgl::Sprite* afterburner = &sprites[5];  //afterburner sprites
 
   int playerVelX = 0;
   int lastAnimUpdate = 0;
@@ -28,14 +29,24 @@ struct GameScene : public Scene {
 
     player->addBitmap(&bitmap1);
     player->addBitmap(&bitmap2);
-    player->addBitmap(&bitmap3);
+    player->addBitmap(&bitmap3);  //Main sprite for player
     player->addBitmap(&bitmap4);
     player->addBitmap(&bitmap5);
 
-    player->moveTo(152, 170);
+    //sprites for afterburner
+    afterburner->addBitmap(&afterburner_0);
+    afterburner->addBitmap(&afterburner_1);
+    afterburner->addBitmap(&afterburner_2);
+    afterburner->addBitmap(&afterburner_3);
+
+    afterburner->moveTo(152 + 4, 170 + 4 - 1);  //test
+
+    player->moveTo(152, 170);  //x,y
     addSprite(player);
+    addSprite(afterburner);
     DisplayController.setSprites(sprites, SPRITESCOUNT);
     player->setFrame(2);
+    afterburner->setFrame(0);
   }
 
   void update(int updateCount) override {
@@ -91,19 +102,33 @@ struct GameScene : public Scene {
 
     // if no keys, return to neutral
     if (!leftPressed && !rightPressed) {
-      
+
       if (updateCount - lastAnimUpdate >= 4) {
         lastAnimUpdate = updateCount;
         int frame = player->getFrameIndex();
         if (frame < 2) player->setFrame(frame + 1);  // advance to right tilt
-        if (frame > 2) player->setFrame(frame - 1);  // advance to right tilt      
-
+        if (frame > 2) player->setFrame(frame - 1);  // advance to right tilt
       }
       //player->setFrame(2);
     }
 
-    // update sprite
+    // animate afterburner
+if (updateCount % 4 == 0) {
+
+  static int lastFrame = 0;
+
+  int newFrame = random(0, 4);
+
+  lastFrame = (newFrame == lastFrame) ? random(0, 4) : newFrame;
+
+  afterburner->setFrame(lastFrame);
+}
+    // keep it attached to player
+    afterburner->x = player->x + 6;
+    afterburner->y = player->y + (16 - 3);
+    // update sprites
     updateSprite(player);
+    updateSprite(afterburner);
     DisplayController.refreshSprites();
   }
 
